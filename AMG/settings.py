eac615a9149d1d973ee129659866cd1143b96819
WinterOdin <cczuryszkiewicz@gmail.com>
2020-10-01 16:50:57 +0200

    added static_root

--- AMG/settings.py
+++ AMG/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/staticRoot')
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
